3bad5ca820a615fe52d6b16526353bcff54cce4d
Saaransh-Mehta <saaransh1621m@gmail.com>
2024-12-08 19:39:37 +0530

    Appwrite setup

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: String(import.meta.env.VITE_API_KEY),
+    Endpoint:String(import.meta.env.VITE_API_ENDPOINT),
+    Database: String(import.meta.env.VITE_DATABASE_ID),
+    Collection : String(import.meta.env.VITE_COLLECTION_ID),
+    Bucket : String(import.meta.env.VITE_BUCKET_ID)
+}
+
+
+export default config
